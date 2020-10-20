6979ee95bbcd69b72cc3db5faec573c6ea3749b6
yangdejun <martonyang0707@163.com>
2020-10-20 15:18:03 +0800

    update someting

---
+++ src/main/java/com/practice/sprngframework/core/exception/TestException.java
@@ -0,0 +1,19 @@
+package com.practice.sprngframework.core.exception;
+
+/**
+ * @author yangdejun
+ * @date 2020/08/27
+ **/
+public class TestException {
+
+    
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+
+        } finally {
+
+        }
+    }
+}
